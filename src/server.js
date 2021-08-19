c8dab0ad234f0632fe436cffa289818cb41b78d7
seongchan <xoom3299@gmail.com>
2021-08-19 17:33:33 +0900

    #1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
@@ -19,12 +20,21 @@ const wss=new WebSocket.Server({server});
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Conneted to Server ✅");
     socket.on("close", ()=>{
         console.log("Disconnect to Server");
     });
-    socket.on("message", (message) => {
-        sockets.forEach((aMessage) => {aMessage.send(message.toString())});
+    socket.on("message", (msg) => {
+        const parsed = JSON.parse(msg.toString());
+        switch (parsed.type){
+            case "nickname" :
+                socket["nickname"] = parsed.payload;
+                break;
+            case "new_message":
+                sockets.forEach((aMessage) => {aMessage.send(`${socket["nickname"]} : ${parsed.payload}`)});
+                break;
+        }
     });
 });
 
