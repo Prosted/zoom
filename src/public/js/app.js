e8365231cdd57af9ba9802bb54ceb7cee0702fc9
seongchan <xoom3299@gmail.com>
2021-08-23 17:15:16 +0900

    #3.0 User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,85 +1,49 @@
 const socket = io();
 
-const welcome = document.querySelector("#welcome");
-const welcomeForm = welcome.querySelector("form");
-const roomList = welcome.querySelector("#roomList");
-const message = document.querySelector("#message");
-const messageForm = message.querySelector("#messageForm");
-const nicknameForm = message.querySelector("#nickname");
-const roomName = message.querySelector("#roomName");
-const messageList = message.querySelector("#messageList");
+const stream = document.querySelector("#myStream");
+const video = stream.querySelector("#video");
+const muteBtn = stream.querySelector("#muteBtn");
+const cameraBtn = stream.querySelector("#cameraBtn");
 
+let myStream;
+let muted = false;
+let cameraOff= false;
 
-let roomTitle;
-message.hidden = true;
-
-const showRoom = () => {
-    welcome.hidden=true;
-    message.hidden=false;
-    messageForm.addEventListener("submit", handleMessageSubmit);
-    nicknameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-const handleNicknameSubmit = (event) => {
-    event.preventDefault();
-    const input = nicknameForm.querySelector("input");
-    const value = input.value;
-    socket.emit("nickname", value);
-    input.value="";
-
-}
-
-const handleMessageSubmit = (event) =>{
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    const value = input.value;
-    socket.emit("new_message", value, roomName.innerText, ()=>{
-        addMessage(`You : ${value}`);
-    })
-    input.value="";
-}   
-
-const handleSubmit = (event) => {   
-    event.preventDefault();
-    console.log("hey");
-    const input = welcomeForm.querySelector("input");
-    const {value} = input;
-    roomTitle=value;
-    roomName.innerText = roomTitle;
-    socket.emit("enter-room", value, showRoom);
-    input.value="";
-}
-
-const addMessage = (msg) => {
-    const li = document.createElement("li");
-    li.innerText = msg;
-    messageList.appendChild(li);
-}
-
-socket.on("welcome", (user, count)=>{
-    roomName.innerText = `${roomTitle} (${count})`;
-    addMessage(`${user} joined`);
-});
-
-socket.on("bye", (user, count)=>{
-    roomName.innerText = `${roomTitle} (${count})`;
-    addMessage(`${user} left ㅠㅠ`);
-})
-
-socket.on("new_message", (msg)=>{
-    addMessage(msg);
-});
-
-socket.on("room-change", (rooms)=>{
-    roomList.innerHTML="";
-    if (rooms.length ===0 ){
-        return;
+async function getStream(){
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia({
+            video:true,
+            audio:true
+        })
+        video.srcObject = myStream;
+    }catch(e){
+        console.log(e);
     }
-    rooms.forEach(room => {
-        const li= document.createElement("li");
-        li.innerText=room;
-        roomList.appendChild(li);
-    });
-})
 
-welcomeForm.addEventListener("submit", handleSubmit);
+}
+
+getStream();
+
+const handleMute = () => {
+    if(!muted){
+        muteBtn.innerText="UnMute";
+        muted=true;
+    }else{
+        muteBtn.innerText="Mute";
+        muted=false;
+    }
+}
+
+const handleCamera = () => {
+    if(!cameraOff){
+        cameraBtn.innerText="Camera ON";
+        cameraOff= true;
+    }else{
+        cameraBtn.innerText="Camera OFF";
+        cameraOff= false;
+    }
+}
+
+muteBtn.addEventListener("click", handleMute);
+cameraBtn.addEventListener("click", handleCamera);
+
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 // import WebSocket from "ws";
 import { instrument } from "@socket.io/admin-ui";
-import {Server} from "socket.io";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -16,93 +16,7 @@ app.get("/*", (req,res)=>{return res.redirect("/")});
 
 
 const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-      origin: ["http://localhost:4000/"],
-      credentials: true
-}});
-
-instrument(io, {
-    auth: false
-});
-  
-
-function publicRooms(){
-    const {
-        sockets:{
-            adapter :{sids, rooms},
-        }
-    } = io;
-    const publicRooms = [];
-    rooms.forEach((_, key)=>{
-        if(sids.get(key)===undefined){
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName){
-    console.log(io.sockets.adapter.rooms.get(roomName)?.size);
-    return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-
-
-io.on("connect", (socket)=>{
-    socket["nickname"]="Anony";
-    socket.onAny((event)=>{
-        console.log(`server event : ${event}`);
-    });
-    socket.on("enter-room", (room, done)=>{
-        socket.join(room);
-        done();
-        socket.emit("welcome", socket["nickname"], countRoom(room));
-        socket.to(room).emit("welcome", socket["nickname"], countRoom(room));
-        io.sockets.emit("room-change", publicRooms());
-    });
-    socket.on("disconnecting", ()=>{
-        socket.rooms.forEach(room => {
-            socket.to(room).emit("bye", socket["nickname"], countRoom(room)-1);
-        });
-    });
-    socket.on("disconnect", ()=>{
-        io.sockets.emit("room-change", publicRooms());
-    })
-    socket.on("new_message", (msg, room, done)=>{
-        socket.to(room).emit("new_message", `${socket["nickname"]} : ${msg}`);
-        done();
-    })
-    socket.on("nickname", (nickname)=>{
-        socket["nickname"]=nickname;
-    });
-})
-
-
-/*
-const wss=new WebSocket.Server({httpServer});
-
-const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Conneted to Server ✅");
-    socket.on("close", ()=>{
-        console.log("Disconnect to Server");
-    });
-    socket.on("message", (msg) => {
-        const parsed = JSON.parse(msg.toString());
-        switch (parsed.type){
-            case "nickname" :
-                socket["nickname"] = parsed.payload;
-                break;
-            case "new_message":
-                sockets.forEach((aMessage) => {aMessage.send(`${socket["nickname"]} : ${parsed.payload}`)});
-                break;
-        }
-    });
-});
-*/
+const io = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:4000`);
 
